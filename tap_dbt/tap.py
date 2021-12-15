a609b38b1b3ca85462e6c947c9c561f7de8876e9
Edgar Ramírez <edgarrm358@gmail.com>
2021-12-15 11:44:06 -0600

    Create User-Agent dynamically from the Tap class (#27)
    * Create User-Agent from plugin class
    
    * Add dynamic user agent

--- tap_dbt/tap.py
+++ tap_dbt/tap.py
@@ -3,6 +3,7 @@
 from typing import List
 
 from singer_sdk import Stream, Tap
+from singer_sdk.helpers._classproperty import classproperty
 from singer_sdk.typing import ArrayType, PropertiesList, Property, StringType
 
 from tap_dbt.streams import AccountsStream, JobsStream, ProjectsStream, RunsStream
@@ -17,15 +18,23 @@ STREAM_TYPES = [
 
 
 class TapDBT(Tap):
-    """dbt tap class."""
+    """Singer tap for the dbt Cloud API."""
 
     name = TAP_NAME
 
-    config_jsonschema = PropertiesList(
-        Property("api_key", StringType, required=True),
-        Property("account_ids", ArrayType(StringType), required=True),
-        Property("base_url", StringType, default="https://cloud.getdbt.com/api/v2"),
-    ).to_dict()
+    @classproperty
+    def config_jsonschema(cls):
+        return PropertiesList(
+            Property("api_key", StringType, required=True),
+            Property("account_ids", ArrayType(StringType), required=True),
+            Property("base_url", StringType, default="https://cloud.getdbt.com/api/v2"),
+            Property(
+                "user_agent",
+                StringType,
+                default=f"{cls.name}/{cls.plugin_version} {cls.__doc__}",
+                description="User-Agent header",
+            ),
+        ).to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
