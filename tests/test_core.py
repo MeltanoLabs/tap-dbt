4d6e2f48c103c8b265dee3d311f644ff93ee5227
mjsqu <mj2.squ@gmail.com>
2023-05-25 14:44:51 +1200

    Add tests for new endpoints

--- tests/test_core.py
+++ tests/test_core.py
@@ -48,36 +48,84 @@ def accounts_response(faker: Faker):
         },
     }
 
-
 @pytest.fixture()
-def projects_response():
-    """Return a sample response for the projects stream."""
+def connections_response(faker: Faker):
+    """Return a sample response for the connections stream."""
     return {
         "status": {
             "code": 200,
             "is_success": True,
         },
-        "data": [
-            {
-                "id": 1000 + i,
-                "account_id": 1000,
-            }
-            for i in range(10)
-        ],
         "extra": {
             "filters": {
-                "account_id": 1,
                 "limit": 1,
                 "offset": 0,
+                "account_id": 1,
             },
             "order_by": "id",
             "pagination": {
                 "count": 1,
-                "total_count": 2,
+                "total_count": 300,
             },
         },
+        "data": [
+            {
+                "created_by_id": 12,
+                "created_by_service_token_id": None,
+                "id": 1,
+                "state": faker.random_element([1, 2]),
+                "account_id": 1000,
+                "dbt_project_id": 1,
+                "name": faker.company(),
+                "type": faker.bs(),
+                "account": faker.bs(),
+                "database": faker.bs(),
+                "warehouse": faker.bs(),
+                "role": faker.bs(),
+                "allow_sso": True
+            }
+        ]
     }
 
+@pytest.fixture()
+def environments_response(faker: Faker):
+    """Return a sample response for the environments stream."""
+    return {
+        "status": {
+            "code": 200,
+            "is_success": True,
+        },
+        "extra": {
+            "filters": {
+                "limit": 1,
+                "offset": 0,
+                "account_id": 1,
+            },
+            "order_by": "id",
+            "pagination": {
+                "count": 1,
+                "total_count": 300,
+            },
+        },
+        "data": [
+            {
+                "id": 1,
+                "account_id": 1,
+                "connection_id": 1,
+                "repository_id": 8,
+                "credentials_id": null,
+                "created_by_id": null,
+                "name": "dev",
+                "use_custom_branch": false,
+                "custom_branch": null,
+                "dbt_version": "1.3.0-latest",
+                "raw_dbt_version": "1.3.0-latest",
+                "supports_docs": false,
+                "state": faker.random_element([1, 2]),
+                "updated_at": "2023-02-06T21:49:24.396847+00:00",
+            }
+        ]
+    }
 
 @pytest.fixture()
 def jobs_response(faker: Faker):
@@ -145,6 +193,71 @@ def jobs_response(faker: Faker):
         ],
     }
 
+@pytest.fixture()
+def projects_response():
+    """Return a sample response for the projects stream."""
+    return {
+        "status": {
+            "code": 200,
+            "is_success": True,
+        },
+        "data": [
+            {
+                "id": 1000 + i,
+                "account_id": 1000,
+            }
+            for i in range(10)
+        ],
+        "extra": {
+            "filters": {
+                "account_id": 1,
+                "limit": 1,
+                "offset": 0,
+            },
+            "order_by": "id",
+            "pagination": {
+                "count": 1,
+                "total_count": 2,
+            },
+        },
+    }
+
+@pytest.fixture()
+def repositories_response(faker: Faker):
+    """Return a sample response for the repositories stream."""
+    return {
+        "status": {
+            "code": 200,
+            "is_success": True,
+        },
+        "extra": {
+            "filters": {
+                "limit": 1,
+                "offset": 0,
+                "account_id": 1,
+            },
+            "order_by": "id",
+            "pagination": {
+                "count": 1,
+                "total_count": 300,
+            },
+        },
+        "data": [
+            {
+                "id": 3,
+                "account_id": 1,
+                "remote_url": faker.file_path(depth=4),
+                "remote_backend": "gitlab",
+                "git_clone_strategy": "deploy_token",
+                "deploy_key_id": 1,
+                "github_installation_id": 1,
+                "pull_request_url_template": faker.url(),
+                "created_at": "2021-05-24T22:17:33.679057+00:00",
+                "updated_at": "2021-05-27T23:47:06.143161+00:00",
+                "state": faker.random_element([1, 2]),
+            }
+        ]
+    }
 
 @pytest.fixture()
 def runs_response():
@@ -177,6 +290,130 @@ def runs_response():
         ],
     }
 
+@pytest.fixture()
+def users_response(faker: Faker):
+    """Return a sample response for the users stream."""
+    return {
+        "status": {
+            "code": 200,
+            "is_success": True,
+        },
+        "extra": {
+            "filters": {
+                "limit": 1,
+                "offset": 0,
+                "account_id": 1,
+            },
+            "order_by": "id",
+            "pagination": {
+                "count": 1,
+                "total_count": 300,
+            },
+        },
+        "data": [
+            {
+                "id": 3,
+                "first_name": faker.first_name(),
+                "last_name": faker.last_name(),
+                "created_at": "2021-02-25T22:49:00.244461+00:00",
+                "last_login": "2023-05-24T20:11:21.637217+00:00",
+                "is_staff": False,
+                "is_active": True,
+                "email": faker.email(),
+                "email_connected": False,
+                "email_verified": True,
+                "github_connected": False,
+                "github_username": None,
+                "gitlab_connected": True,
+                "gitlab_username": f"{faker.first_name()}.{faker.last_name()}",
+                "azure_active_directory_connected": False,
+                "azure_active_directory_username": None,
+                "slack_connected": False,
+                "enterprise_connected": False,
+                "enterprise_authentication_method": None,
+                "auth_provider_infos": {
+                    "sso-azure": {
+                        "domain": faker.domain_name(),
+                        "groups": [
+                        ],
+                        "auth_provider_type": "azure_single_tenant"
+                    }
+                },
+                "permissions": [
+                    {
+                        "license_type": "developer",
+                        "id": 3,
+                        "user_id": 3,
+                        "account_id": 1000,
+                        "state": faker.random_element([1, 2]),
+                        "created_at": "2021-02-25T22:49:00.276363+00:00",
+                        "updated_at": "2023-05-24T20:11:21.511698+00:00",
+                        "groups": [
+                            {
+                                "account_id": 1,
+                                "name": "Everyone",
+                                "id": 3,
+                                "state": 1,
+                                "assign_by_default": True,
+                                "sso_mapping_groups": [],
+                                "created_at": "2021-02-24 16:29:13.116664+00:00",
+                                "updated_at": "2021-02-24 16:29:13.116676+00:00",
+                                "group_permissions": []
+                            },
+                            {
+                                "account_id": 1000,
+                                "name": faker.bs(),
+                                "id": 13,
+                                "state": 1,
+                                "assign_by_default": False,
+                                "sso_mapping_groups": [
+                                    faker.bs()
+                                ],
+                                "created_at": "2021-10-19 21:15:13.346836+00:00",
+                                "updated_at": "2022-05-05 05:17:48.090351+00:00",
+                                "group_permissions": [
+                                    {
+                                        "account_id": 1,
+                                        "group_id": 13,
+                                        "project_id": None,
+                                        "all_projects": True,
+                                        "permission_set": faker.bs(),
+                                        "permission_level": None,
+                                        "id": 13,
+                                        "state": 1,
+                                        "created_at": "2021-10-19 21:15:13.668908+00:00",
+                                        "updated_at": "2022-05-05 05:17:48.273425+00:00"
+                                    }
+                                ]
+                            }
+                        ],
+                        "permission_statements": [
+                            {
+                                "permission": "custom_environment_variables_write",
+                                "target_resource": None,
+                                "all_resources": True
+                            }
+                        ]
+                    }
+                ],
+                "licenses": {
+                    "1": {
+                        "license_type": faker.bs(),
+                        "id": 3,
+                        "user_id": 3,
+                        "account_id": 1000,
+                        "state": faker.random_element([1, 2]),
+                        "created_at": "2021-02-25T22:49:00.276363Z",
+                        "updated_at": "2023-05-24T20:11:21.511698Z"
+                    }
+                },
+                "gitlab_token_retrieval_failure": False,
+                "avatar_url": None,
+                "fullname": faker.name(),
+                "show_existing_user_email_verification": False
+            }
+        ]
+    }
 
 @responses.activate
 def test_standard_tap_tests(
@@ -197,8 +434,14 @@ def test_standard_tap_tests(
 
     responses.add(
         responses.GET,
-        "https://cloud.getdbt.com/api/v2/accounts/1000/projects",
-        json=projects_response,
+        "https://cloud.getdbt.com/api/v2/accounts/1000/connections",
+        json=connections_response,
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        "https://cloud.getdbt.com/api/v2/accounts/1000/environments",
+        json=environments_response,
         status=200,
     )
 
@@ -211,8 +454,29 @@ def test_standard_tap_tests(
 
     responses.add(
         responses.GET,
+        "https://cloud.getdbt.com/api/v2/accounts/1000/projects",
+        json=projects_response,
+        status=200,
+    )
+
+    responses.add(
+        responses.GET,
+        "https://cloud.getdbt.com/api/v2/accounts/1000/repositories",
+        json=respositories_response,
+        status=200,
+    )
+
+    responses.add(
+        responses.GET,
         "https://cloud.getdbt.com/api/v2/accounts/1000/runs",
         json=runs_response,
+        status=200,
+    )
+
+    responses.add(
+        responses.GET,
+        "https://cloud.getdbt.com/api/v2/accounts/1000/users",
+        json=users_response,
         status=200,
     )
 
